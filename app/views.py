730a268101b577d996bff04b2014130d3affb367
Asadbek <asadbeko422gmail.com>
2023-10-13 20:46:07 +0500

    finished index

--- app/views.py
+++ app/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from .models import About, Guarantee, Service, Feature, Doctor, Testimonial
 
 def main(request):
+    context = {
+        'abouts':About.objects.all(),
+        'guarantees':Guarantee.objects.all(),
+        'services':Service.objects.all(),
+        'features':Feature.objects.all(),
+        'doctors':Doctor.objects.all(),
+        'testimonials':Testimonial.objects.all(),
+    }
     if not request.user.is_authenticated:
         return redirect('/login/')
     else:
         if request.POST:
             pass
-        return render(request,'index.html')
+        return render(request,'index.html',context)
 
 def login_u(request):
     if request.POST:
